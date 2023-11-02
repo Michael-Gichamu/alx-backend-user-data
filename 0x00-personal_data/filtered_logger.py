0f39751862592848661729e88b15aaff35922f46
Michael-Gichamu <michaelmatere606@gmail.com>
2023-11-02 23:09:40 +0300

    Personal Data Obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,17 +2,19 @@
 """Contains filter_datum function."""
 import re
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
-    """_summary_
 
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
+    """
     Args:
-        fields (list):  strings representing all fields to obfuscate
-        redaction (str): string representing by what the field will be obfuscated
+        fields (list): All fields to obfuscate
+        redaction (str): field to be obfuscated
         message (str): Log line
-        separator (str): Character is separating all fields in the log line (message)
+        separator (str): Character is separating all fields in the log line.
 
     Returns:
         str: log message obfuscated.
     """
     regex_pattern = f"({'|'.join(fields)})=[^{separator}]+"
-    return re.sub(regex_pattern, lambda x: x.group().split('=')[0] + '=' + redaction, message)
+    return re.sub(regex_pattern, lambda x: x.group().split('=')[0] +
+                  '=' + redaction, message)
